acde161ab1dfb3a9c64b93d07db8e39ded3fccaa
fgq233 <18217610763@163.com>
2024-05-12 20:42:11 +0800

    Flowable 6.7.2

--- src/main/java/com/fgq/demo/listener/MyTaskListener.java
+++ src/main/java/com/fgq/demo/listener/MyTaskListener.java
@@ -4,14 +4,14 @@ package com.fgq.demo.listener;
 import org.flowable.task.service.delegate.DelegateTask;
 import org.flowable.task.service.delegate.TaskListener;
 
-public class UserListener implements TaskListener {
+public class MyTaskListener implements TaskListener {
 
     @Override
     public void notify(DelegateTask delegateTask) {
         System.out.println("触发UserListener");
         if (EVENTNAME_CREATE.equals(delegateTask.getEventName())) {
             System.out.println("任务节点创建事件触发，指定处理人");
-            delegateTask.setAssignee("DDD");
+            delegateTask.setAssignee("fgq");
         }
     }
 }
